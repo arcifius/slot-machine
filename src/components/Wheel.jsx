cfae4cdc34747f4973c9bd9ebc25017d35502682
Augusto <augustopaladin@gmail.com>
2019-04-30 17:17:13 -0300

    Fixed image loading and updated README

--- src/components/Wheel.jsx
+++ src/components/Wheel.jsx
@@ -1,12 +1,5 @@
 import React from "react";
 import PropTypes from "prop-types";
-import LazyLoad from "react-lazyload";
-
-// Assets
-import strawberry from "assets/strawberry.svg";
-import banana from "assets/banana.svg";
-import orange from "assets/orange.svg";
-import monkey from "assets/monkey.svg";
 
 import "styles/components/Wheel.scss";
 
@@ -14,28 +7,22 @@ function Wheel(props) {
   function getCurrent() {
     switch (props.current) {
       case 1:
-        return { alt: "banana", src: banana };
+        return "banana";
 
       case 2:
-        return { alt: "orange", src: orange };
+        return "orange";
 
       case 3:
-        return { alt: "monkey", src: monkey };
+        return "monkey";
 
       default:
-        return { alt: "strawberry", src: strawberry };
+        return "strawberry";
     }
   }
 
   const current = getCurrent();
 
-  return (
-    <div className="Wheel">
-      <LazyLoad height={50} once>
-        <img src={current.src} className="Wheel-slot-icon" alt={current.alt} />
-      </LazyLoad>
-    </div>
-  );
+  return <div className={`Wheel ${current}`} data-testid={current} />;
 }
 
 Wheel.propTypes = {
--- src/tests/components/Wheel.test.js
+++ src/tests/components/Wheel.test.js
@@ -6,9 +6,9 @@ describe("Slot machine wheel", () => {
   afterEach(cleanup);
 
   it("renders strawberry when current is 0 or greather than 3", () => {
-    const { getByAltText, rerender } = render(<Wheel current={0} />);
+    const { getByTestId, rerender } = render(<Wheel current={0} />);
 
-    const strawberry = getByAltText("strawberry");
+    const strawberry = getByTestId("strawberry");
 
     expect(strawberry).toBeDefined();
 
@@ -18,25 +18,25 @@ describe("Slot machine wheel", () => {
   });
 
   it("renders banana when current is 1", () => {
-    const { getByAltText } = render(<Wheel current={1} />);
+    const { getByTestId } = render(<Wheel current={1} />);
 
-    const banana = getByAltText("banana");
+    const banana = getByTestId("banana");
 
     expect(banana).toBeDefined();
   });
 
   it("renders orange when current is 2", () => {
-    const { getByAltText } = render(<Wheel current={2} />);
+    const { getByTestId } = render(<Wheel current={2} />);
 
-    const orange = getByAltText("orange");
+    const orange = getByTestId("orange");
 
     expect(orange).toBeDefined();
   });
 
   it("renders monkey when current is 3", () => {
-    const { getByAltText } = render(<Wheel current={3} />);
+    const { getByTestId } = render(<Wheel current={3} />);
 
-    const monkey = getByAltText("monkey");
+    const monkey = getByTestId("monkey");
 
     expect(monkey).toBeDefined();
   });
